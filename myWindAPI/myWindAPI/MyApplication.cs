d54a4b054894ae93873f0a58f78243a58233e30a
heng mao <184772181@qq.com>
2016-04-20 16:39:12 +0800

    更新

---
+++ myWindAPI/myWindAPI/MyApplication.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace myWindAPI
+{
+    class MyApplication
+    {
+        /// <summary>
+        /// 去掉字符串中的数字
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string RemoveNumber(string key)
+        {
+            return System.Text.RegularExpressions.Regex.Replace(key, @"\d", "");
+        }
+
+        /// <summary>
+        /// 去掉字符串中的非数字
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string RemoveNotNumber(string key)
+        {
+            return System.Text.RegularExpressions.Regex.Replace(key, @"[^\d]*", "");
+        }
+    }
+}
